b22d94a9b987963ef00722285320500d49a1b50b
seweryn17723 <161207556+seweryn17723@users.noreply.github.com>
2024-03-03 10:21:27 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+
+print(sys.version)
+
+a = 56
+print(a)
+print(type(a))
+b = 5.5
+print(type(b))
+zmienna_tekstowa = 'wizualizacja danych'
+print(zmienna_tekstowa)
+print(type(zmienna_tekstowa))
